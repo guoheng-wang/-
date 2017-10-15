7891273a962d4a182f60f850171f962884cf0de6
guoheng-wang <31719707+guoheng-wang@users.noreply.github.com>
2017-10-15 23:16:48 +0800

    Add files via upload

---
+++ 4_3.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream.h>
+main()
+{
+	int x,i=0;
+    cout<<"please input: ";
+    cin>>x;
+    while(x!=1)
+	{
+		if(x%2==0) 
+			x=x/2;
+		else 
+			x=3*x+1;
+		i++;
+	}
+	cout<<i<<endl;
+}
